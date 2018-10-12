5e1988c77df1bc94be7ccc76fd6d12c7e74ca1dd
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2018-10-12 09:35:43 +0200

    remove implicit user creation from JamboreeService

--- src/main/kotlin/com/beersound/beersoundbackend/service/implementaion/JamboreeServiceImpl.kt
+++ src/main/kotlin/com/beersound/beersoundbackend/service/implementaion/JamboreeServiceImpl.kt
@@ -2,14 +2,13 @@ package com.beersound.beersoundbackend.service.implementaion
 
 import com.beersound.beersoundbackend.dto.JamboreeDto
 import com.beersound.beersoundbackend.dto.NewJamboreeDto
-import com.beersound.beersoundbackend.entity.BeerSoundUser
-import com.beersound.beersoundbackend.entity.Jamboree
 import com.beersound.beersoundbackend.repository.JamboreeRepository
 import com.beersound.beersoundbackend.repository.UserRepository
 import com.beersound.beersoundbackend.service.JamboreeService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityNotFoundException
 
 @Service
 @Transactional
@@ -19,34 +18,15 @@ class JamboreeServiceImpl @Autowired constructor(
 ) : JamboreeService {
 
     override fun createJamboree(externalUserId: String, jamboree: NewJamboreeDto): JamboreeDto {
-        val user = userRepository.findByExternalId(externalUserId) ?: userRepository.save(
-                BeerSoundUser(
-                        id = null,
-                        externalId = externalUserId,
-                        displayName = externalUserId,
-                        pictureUri = null,
-                        controlledJamborees = mutableListOf(),
-                        jamborees = mutableListOf(),
-                        tracks = mutableListOf()
-                )
-        )
+        val user = userRepository.findByExternalId(externalUserId)
+                ?: throw EntityNotFoundException("User with external id = $externalUserId not found")
         val createdJamboree = jamboreeRepository.save(jamboree.toEntity(user))
         return createdJamboree.toDto()
     }
 
     override fun enterJamboree(externalUserId: String, code: String): JamboreeDto? {
-        // FIXME
-        val user = userRepository.findByExternalId(externalUserId) ?: userRepository.save(
-                BeerSoundUser(
-                        id = null,
-                        externalId = externalUserId,
-                        displayName = externalUserId,
-                        pictureUri = null,
-                        controlledJamborees = mutableListOf(),
-                        jamborees = mutableListOf(),
-                        tracks = mutableListOf()
-                )
-        )
+        val user = userRepository.findByExternalId(externalUserId)
+                ?: throw EntityNotFoundException("User with external id = $externalUserId not found")
 
         val jamboree = jamboreeRepository.findByCode(code)
 
