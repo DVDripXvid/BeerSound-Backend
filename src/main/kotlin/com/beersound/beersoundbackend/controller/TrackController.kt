556d6043c5ba68f004778023fff1240fd5f2d830
Adam Kassai <adamkassai@gmail.com>
2018-10-17 22:51:38 +0200

    getTracksByJamboree and addTrackToJamboree endpoints added

--- src/main/kotlin/com/beersound/beersoundbackend/controller/TrackController.kt
+++ src/main/kotlin/com/beersound/beersoundbackend/controller/TrackController.kt
@@ -2,21 +2,21 @@ package com.beersound.beersoundbackend.controller
 
 import com.beersound.beersoundbackend.dto.BeerSoundTrackDto
 import com.beersound.beersoundbackend.dto.NewBeerSoundTrackDto
+import com.beersound.beersoundbackend.security.userAttrName
+import com.beersound.beersoundbackend.service.TrackService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController()
 @RequestMapping("api/jamborees/{jamboreeId}/tracks")
-class TrackController {
+class TrackController @Autowired constructor(private val trackService: TrackService) {
 
     @GetMapping
-    fun getTracksByJamboree(jamboreeId: Int): List<BeerSoundTrackDto> {
-        throw NotImplementedError()
-    }
-
+    fun getTracksByJamboree(@PathVariable jamboreeId: Int): List<BeerSoundTrackDto>
+        = trackService.getTracksByJamboree(jamboreeId)
     @PostMapping
-    fun addTrackToJamboree(jamboreeId: Int, @RequestBody track: NewBeerSoundTrackDto) {
-        throw NotImplementedError()
-    }
+    fun addTrackToJamboree(@RequestAttribute(userAttrName) externalUserId: String, @PathVariable jamboreeId: Int, @RequestBody track: NewBeerSoundTrackDto)
+        = trackService.addTrackToJamboree(externalUserId, jamboreeId, track)
 
     @DeleteMapping("/{trackId}")
     fun removeTrackFromJamboree(jamboreeId: Int, trackId: Int) {
